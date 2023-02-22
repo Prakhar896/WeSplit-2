0ea80f95a509f82a5040af934c311aeb0e6b27db
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-02-22 18:03:36 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Prakhar Trivedi on 22/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
